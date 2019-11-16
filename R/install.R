e17546714a8f7b533fa0cbf33f7a3c5ef1c0837d
Javier Luraschi <javierluraschi@hotmail.com>
2019-11-15 16:52:24 -0800

    support installing gpu version

--- R/install.R
+++ R/install.R
@@ -2,10 +2,15 @@
 #'
 #' Installs all 'mlverse' runtime dependencies.
 #'
+#' @param version The version to install. Currently \code{"auto"} or
+#'   \code{"gpu"}.
+#'
 #' @export
-install_mlverse <- function() {
+install_mlverse <- function(version = c("auto", "gpu")) {
+  tensorflow_version <- if (identical(version, "gpu")) "gpu" else NULL
+
   sparklyr::spark_install(version = "2.3")
-  tensorflow::install_tensorflow()
+  tensorflow::install_tensorflow(version = tensorflow_version)
   keras::install_keras()
   mlflow::install_mlflow()
 }
